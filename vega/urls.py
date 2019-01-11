25475bb864318e9e42bb658217361935678396f4
huynamvn <huynamvn1606@gmail.com>
2019-01-11 09:47:08 +0700

    rename app

--- vega/urls.py
+++ vega/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from vega1 import views
+from app_panelapi import views
 
 
 urlpatterns = [
--- worker.py
+++ worker.py
@@ -2,25 +2,10 @@ import logging
 import requests
 import pymysql
 import pymysql.cursors
-import json
-import urllib
 
-
-from django_cron import CronJobBase, Schedule
 
 from datetime import datetime
 
-# Create your views here.
-
-#
-# class MyCronJob ( CronJobBase ):
-#     RUN_EVERY_MINS = 120 # every 2 hours
-#
-#     schedule = Schedule( run_every_mins = RUN_EVERY_MINS )
-#     code = 'my_app.my_cron_job'    # a unique code
-#
-#     def do ( self ):
-#         pass    # do your thing here
 
 
 def table():
